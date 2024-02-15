5a029c94fa990b29ab98f26f8723c05cf48ecb83
Bini0104 <goqsldl0120@gmail.com>
2024-02-15 15:40:50 +0900

    Initial commit

---
+++ 01_SELECT.sql
@@ -0,0 +1,24 @@
+/* select */
+
+SELECT 
+    menu_name
+FROM
+    tbl_menu;
+       
+SELECT 
+    MENU_CODE, MENU_NAME, MENU_PRICE
+FROM
+    TBL_MENU;
+  
+SELECT 
+    *
+FROM
+    TBL_MENU;
+    
+SELECT 6 + 3;
+SELECT 6 * 3;
+SELECT 6 % 3;
+
+SELECT NOW();
+SELECT CONCAT('홍',' ', '길동') AS name;
+SELECT CONCAT('홍',' ', '길동') AS 'FUll name';
